050726ffedc44ec846f4cb430ec367e5b6f39a37
shim <shim@yizhenmoney.com>
2016-06-27 16:57:18 +0800

    app

--- damocles-configcenter-console/src/main/java/com/yizhenmoney/damocles/configcenter/CcMain.java
+++ damocles-configcenter-console/src/main/java/com/yizhenmoney/damocles/configcenter/CcMain.java
@@ -9,7 +9,7 @@ public class CcMain {
 		tomcat.setPort(8085);
 		tomcat.getConnector().setURIEncoding("UTF-8");
 		String path = CcMain.class.getResource("/").getPath();
-		tomcat.addWebapp("/", path.substring(0, path.indexOf("target")) + "src/main/webapp");
+		tomcat.addWebapp("/cc", path.substring(0, path.indexOf("target")) + "src/main/webapp");
 		tomcat.getConnector().setMaxPostSize(6291456);
 		tomcat.getConnector().setMaxSavePostSize(6291456);
 		tomcat.start();
