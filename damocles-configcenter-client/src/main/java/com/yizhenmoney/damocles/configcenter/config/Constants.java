06df68c86dbe9e23e7cf6b72d4c4838f8691ad6a
shiming <daviding2002@sina.com>
2016-09-05 13:42:40 +0800

    Update Constants.java

--- damocles-configcenter-client/src/main/java/com/yizhenmoney/damocles/configcenter/config/Constants.java
+++ damocles-configcenter-client/src/main/java/com/yizhenmoney/damocles/configcenter/config/Constants.java
@@ -3,7 +3,7 @@ package com.yizhenmoney.damocles.configcenter.config;
 public class Constants {
 
 	public static final String READ_AUTH = "cc:123456";
-	public static final String SECRET_KEY = "3edc$RFV";
+	public static final String SECRET_KEY = "abcdefg";
 	public static final String PROPERTY_PATH = "/property";
 	public static final String ORIGIN_PATH = "/origin";
 	public static final String DIGEST = "digest";
